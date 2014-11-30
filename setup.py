9d4dbeb6e7fe683fa00ed891257afb7f6397b3d9
Jon Wolverton <wolverton.jr@gmail.com>
2014-11-29 18:29:53 -0800

    final setup for PyPI

--- setup.py
+++ setup.py
@@ -16,7 +16,10 @@ setup(
     version='0.1.0',
     description='GenSON is a powerful, user-friendly JSON Schema generator.',
     long_description=get_long_docs('README.rst', 'HISTORY.rst', 'AUTHORS.rst'),
+    keywords=['json', 'schema', 'object', 'generate', 'generator', 'builder',
+              'draft 4', 'validate', 'validation'],
     url='https://github.com/wolverdude/genson/',
+    download_url='https://github.com/wolverdude/GenSON/tarball/v0.1.0',
     license='MIT',
     author='Jon Wolverton',
     author_email='wolverton (dot) jr (at) gmail (dot) com',
@@ -38,5 +41,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ],
 )
