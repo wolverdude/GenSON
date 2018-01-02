d99abbd43c073b3959ccae539045567a2158d346
Jon Wolverton <wolverton.jr@gmail.com>
2018-01-02 11:46:35 -0500

    start renaming Genson class => SchemaBuilder

--- genson/__init__.py
+++ genson/__init__.py
@@ -1,3 +1,3 @@
-from .schema.root import Genson, Schema
+from .schema.builder import Genson, Schema
 from .schema.node import SchemaNode, SchemaGenerationError
 __all__ = ['Genson', 'SchemaNode', 'SchemaGenerationError', 'Schema']
